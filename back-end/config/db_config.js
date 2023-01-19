b3f352d12bad80d554591512f52b6a90e985e1bb
Annefedy <annecarine1997@gmail.com>
2023-01-18 21:28:56 -0300

    update

--- back-end/config/db_config.js
+++ back-end/config/db_config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    host: "127.0.0.1",
     database: "movies",
     user: "root",
     password: "",
--- front-end/src/config/keys.js
+++
@@ -1,5 +0,0 @@
-const keys = { 
-    apiKey: '6dfb1aec77c621eea6fed3193e322e42'
-}
-  
-export default keys;
